43c89cc14c438d791108a01a2d36291157f1220d
Makar <makardovgopolji@gmail.com>
2025-12-28 12:58:35 +0100

    chore: middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { type MiddlewareConsumer, Module, type NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ActorModule } from './actor/actor.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggingMiddleware } from './common/middleware/logger.middleware';
 import { MovieModule } from './movie/movie.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ReviewModule } from './review/review.module';
@@ -12,9 +13,12 @@ import { ReviewModule } from './review/review.module';
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
-      // load: [configuration],
     }), PrismaModule, ActorModule, ReviewModule, MovieModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
