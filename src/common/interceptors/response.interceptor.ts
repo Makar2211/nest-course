c9efb06c2cb77a668e857f82f1942609de3f0444
Makar <makardovgopolji@gmail.com>
2025-12-28 14:46:30 +0100

    chore: basic interceptors example

---
+++ src/common/interceptors/response.interceptor.ts
@@ -0,0 +1,18 @@
+import { CallHandler, type ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import { map, Observable } from "rxjs";
+
+@Injectable()
+export class ResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next
+      .handle()
+      .pipe(
+        map(data => (
+          {
+            status: 'success',
+            data,
+          }
+        )),
+      );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new ResponseInterceptor())
 
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
